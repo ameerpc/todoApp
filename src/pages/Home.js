644b7d7eb4e63e1e784d7f129fb34d4bc3c08f17
ameerpc <ameerpc9676@gmail.com>
2022-05-17 09:19:06 +0530

    Home page modify

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import styles from './home.module.css'
+import homeImg from '../assets/todo_home_img.png'
 
 const Home = () => {
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.banner}>Welcome to Todo App</div>
+                <div className={styles.banner_img}>
+                    <img src={homeImg} alt="home" className={styles.img} />
+                </div>
+                <div className={styles.banner_text}>
+                    <h1 className={styles.title}>Welcome to ToDo Web Application</h1>
+                    <h3 className={styles.subtitle}>Enumerate you'r dialy tasks here</h3>
+                </div>
             </div>
         </>
     )
